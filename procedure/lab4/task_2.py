17ba99872f0fb50d815b92d59d31e638bb4cfa6c
hokinhim <hokinhimGitHub@gmail.com>
2023-12-17 05:40:12 +0300

    Add files via upload

---
+++ procedure/lab4/task_2.py
@@ -0,0 +1,20 @@
+# TODO импортировать необходимые молули
+import csv
+import json
+
+INPUT_FILENAME = "input.csv"
+OUTPUT_FILENAME = "output.json"
+
+
+def task() -> None:
+    with open(INPUT_FILENAME) as f_in, open(OUTPUT_FILENAME, 'w') as f_out:
+        json.dump([row for row in csv.DictReader(f_in)], f_out, indent=4)
+
+
+if __name__ == '__main__':
+    # Нужно для проверки
+    task()
+
+    with open(OUTPUT_FILENAME) as output_f:
+        for line in output_f:
+            print(line, end="")
